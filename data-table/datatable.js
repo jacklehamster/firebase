468295f836333732763304a2ea713ae46d53c480
Jack Le Hamster <jacklehamster@gmail.com>
2015-02-18 17:05:26 -0800

    Update datatable.js

--- data-table/datatable.js
+++ data-table/datatable.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
     var json = []; 
     addBlankCells(json);
     firebase = new Firebase('https://firelang.firebaseio.com/translations/');
-    resetColumns();
+    resetColumns(json);
 });
 
 function addBlankCells(json) {
     json.push({"":""});
 }
 
-function resetColumns() {
+function resetColumns(json) {
     $('#columns').columns({data:json});
     $(".ui-table").find("thead")[0].addEventListener("mousedown",function() {alert(123);});
 }
