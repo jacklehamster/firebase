79399a98f6d3d378f6b174805f7b9c10c774a63a
Vincent Le Quang <vlequang@adobe.com>
2015-02-26 17:59:20 -0800

    Fixing header

--- styling/header.js
+++ styling/header.js
@@ -2,6 +2,7 @@ if(window==top) {
     window.addEventListener("load",
         function(e) {
             e.currentTarget.removeEventListener(e.type,arguments.callee);
+            var loc = window.location.href.split("firebase/")[1].split("/")[0];
             var table = document.createElement("table");
             table.width="100%";
             table.border = 0;
@@ -12,7 +13,8 @@ if(window==top) {
             tr.appendChild(td);
             td.width = "33%";
             td.style.verticalAlign = "top";
-            td.innerHTML = '[<a href="..">&lt;&lt; Back to Firebase Projects</a>]';
+            td.innerHTML = '[<a href="..">&lt;&lt; Back to Firebase Projects</a>]'
+            + '<br>' + 'source code: <a href="'+loc+'">'+loc+"</a>";
             //  Mid cell
             var td = document.createElement("td");
             tr.appendChild(td);
