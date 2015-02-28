96b40d83911756d5f5d7fddcf34bab23ea7114aa
Jack Le Hamster <jacklehamster@gmail.com>
2015-02-27 17:33:30 -0800

    Update imagereader.js

--- image-reader/imagereader.js
+++ image-reader/imagereader.js
@@ -13,6 +13,7 @@ function attachFirebase(image,firebaseLocation,options) {
              if(preSplit.length>=3 && postSplit.length>=3) {
                  var preTime = parseInt(preSplit[2]);
                  var postTime = parseInt(postSplit[2]);
+                 console.log(postTime);
                  if((preTime - postTime + 100000)%100000 < 10000) {
                      // if preTime is after postTime by less than 10 second, discard postTime
                      return;
