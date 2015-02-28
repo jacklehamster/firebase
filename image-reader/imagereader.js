63f49c9b0e483e2c1983c10cd5e2f54cec9d51ac
Jack Le Hamster <jacklehamster@gmail.com>
2015-02-27 17:39:20 -0800

    Update imagereader.js

--- image-reader/imagereader.js
+++ image-reader/imagereader.js
@@ -10,15 +10,14 @@ function attachFirebase(image,firebaseLocation,options) {
          var postSplit = o.split(";");
          if(options.chrono) {
              var preSplit = image.src.split(";");
-             console.log(preSplit.length,postSplit.length);
-             if(preSplit.length>=3 && postSplit.length>=3) {
-                 var preTime = parseInt(preSplit[2]);
+             if(postSplit.length>=3) {
+                 var preTime = image.preTime ? image.preTime : 0;
                  var postTime = parseInt(postSplit[2]);
-                 console.log(postTime);
                  if((preTime - postTime + 100000)%100000 < 10000) {
                      // if preTime is after postTime by less than 10 second, discard postTime
                      return;
                  }
+                 image.preTime = postTime;
              }
          }
          image.src = postSplit.slice(0,2).join(";");
