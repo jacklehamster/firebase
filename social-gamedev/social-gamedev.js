ba8ea9d4be14f741fdfce509a878d25425669c0e
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-05 11:23:33 -0800

    Update social-gamedev.js

--- social-gamedev/social-gamedev.js
+++ social-gamedev/social-gamedev.js
@@ -668,7 +668,7 @@ function startUpdate() {
 function updateCanvas(canvas) {
    var ctx = canvas.getContext("2d");
    ctx.clearRect(0,0,128,128);
-   ctx.drawImage(canvas.img,0,0,canvas.img.naturalWidth,canvas.img.naturalHeight,0,0);
+   ctx.drawImage(canvas.img,0,0,canvas.img.naturalWidth,canvas.img.naturalHeight,0,0,canvas.width,canvas.height);
    var commands = canvas.strokes;
    if(!commands.length)
         return;
