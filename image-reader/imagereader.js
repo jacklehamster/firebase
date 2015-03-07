3fe3b1ad470dd0f9271706497b21955991a9f4ae
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-06 20:55:34 -0800

    Update imagereader.js

--- image-reader/imagereader.js
+++ image-reader/imagereader.js
@@ -11,7 +11,7 @@ function attachFirebase(image,firebaseLocation,options) {
    image.firebase.on('value',
       image.firebaseRefresh = function(snapshot) {
          var o = snapshot.val();
-         if(!typeof(o)=="string") return;
+         if(typeof(o)!="string") return;
          var postSplit = o.split(";");
          if(options.chrono) {
              var preSplit = image.src.split(";");
