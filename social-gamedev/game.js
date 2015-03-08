abc034aea99f463cedfcae80448b020320341be9
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-08 16:14:59 -0700

    Update game.js

--- social-gamedev/game.js
+++ social-gamedev/game.js
@@ -84,7 +84,7 @@ function shootLaserBeam(x,y,direction) {
   img.src = beamDataURI;
   img.style.position = "absolute";
   img.direction = direction;
-  img.pos = {x:x,y:y-20+10*Math.random()};
+  img.pos = {x:x,y:y-2+Math.random()};
   img.readonly= true;
   lasers[img.id] = img;
   document.getElementById("screen").appendChild(img);
