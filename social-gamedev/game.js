7fb2a498c203af8317a27710d91a3ca00ab87377
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-08 15:00:51 -0700

    Update game.js

--- social-gamedev/game.js
+++ social-gamedev/game.js
@@ -3,6 +3,7 @@ window.addEventListener("load",initGame);
 var dok;
 function initGame() {
   dok = createSprite(dobukiDataURI);
+  dok.style.position = "absolute";
   changeZoom(dok,.5);
   dok.pos = {x:0,y:0};
   document.getElementById("screen").appendChild(dok);
