d075977af5f541c194aafa6f3b41c99358bf3a47
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-08 15:27:08 -0700

    Update social-gamedev.js

--- social-gamedev/social-gamedev.js
+++ social-gamedev/social-gamedev.js
@@ -592,7 +592,7 @@ function findClosestXY(mouseX,mouseY,offsetX,offsetY) {
 
 function convertToScreen(x,y) {
     var scale = calculateScale(-shiftY+y);
-    var valY = (((-shiftY + y)*32)*scale + screenHeight*3/4);
+    var valY = (((-shiftY + y)*32)*scale + screenHeight/2);
     var valX = (((-shiftX + x)*32)*scale + screenWidth/2);
     return {x:valX,y:valY};
 }
