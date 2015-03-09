87ae45acf9d2a8ce887110451269b76e941f4042
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-09 01:02:03 -0700

    Update game.js

--- social-gamedev/game.js
+++ social-gamedev/game.js
@@ -127,33 +127,36 @@ function enterFrame() {
     return;
   globalFrame++;
   var doUpdateScreen = false;
-  var speed = .5;
-  var dx = 0, dy = 0;
-  if(keys[37]) dx--;  //  left
-  if(keys[39]) dx++;  //  right
-  if(keys[38]) dy--;  //  up
-  if(keys[40]) dy++;  //  down
   
-  if(dx) {
-    dok.setDirection(dx);
-  }
-  
-  if(dx!=0 || dy!=0) {
-    if(dok.label!="running")
-      dok.gotoAndPlay("running")
-    dok.pos.x += dx*speed;
-    dok.pos.y += dy*speed;
-    doUpdateScreen = true;
-  }
-  else {
-    if(dok.label!="still")
-      dok.gotoAndPlay("still");
-  }
-  
-  //  shoot laser
-  if(keys[32]) {  //  space bar
-    if(globalFrame-dok.lastLaser>5) {
-      shootLaserBeam(dok.pos.x,dok.pos.y,dok.direction,2);
+  if(!dok.ko) {
+    var speed = .5;
+    var dx = 0, dy = 0;
+    if(keys[37]) dx--;  //  left
+    if(keys[39]) dx++;  //  right
+    if(keys[38]) dy--;  //  up
+    if(keys[40]) dy++;  //  down
+    
+    if(dx) {
+      dok.setDirection(dx);
+    }
+    
+    if(dx!=0 || dy!=0) {
+      if(dok.label!="running")
+        dok.gotoAndPlay("running")
+      dok.pos.x += dx*speed;
+      dok.pos.y += dy*speed;
+      doUpdateScreen = true;
+    }
+    else {
+      if(dok.label!="still")
+        dok.gotoAndPlay("still");
+    }
+    
+    //  shoot laser
+    if(keys[32]) {  //  space bar
+      if(globalFrame-dok.lastLaser>5) {
+        shootLaserBeam(dok.pos.x,dok.pos.y,dok.direction,2);
+      }
     }
   }
   
