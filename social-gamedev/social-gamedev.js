357f4e38c961d7c9814659ef5dce600863e596a7
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-08 21:57:37 -0700

    Update social-gamedev.js

--- social-gamedev/social-gamedev.js
+++ social-gamedev/social-gamedev.js
@@ -628,6 +628,7 @@ function ensureImage(img,ignoreSrc,ignoreAdd) {
 function checkFirebaseAttachment(img) {
     if(!img.firebase && img.path) {
         var screenPos = convertToScreen(img.pos.x,img.pos.y);
+        console.log(screenPos,screenWidth,screenHeight)
         if(screenPos.x>0 && screenPos.y>0 && screenPos.x<screenWidth && screenPos.y<screenHeight) {
             var firebaseSrc = firebaseImg.child(img.path).child("src");
             firebaseSrc.set(img.src);
