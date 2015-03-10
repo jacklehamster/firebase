3c66f0ae9eed3f4323448bcbf146a3f09bc30cd3
Jack Le Hamster <jacklehamster@gmail.com>
2015-03-09 21:50:58 -0700

    Update game.js

--- social-gamedev/game.js
+++ social-gamedev/game.js
@@ -140,7 +140,7 @@ function updateScore() {
 }
 
 function invincible(img) {
-   return img.born && globalFrame-img.born<50;
+   return globalFrame-(img.born?img.born:0)<50;
 }
 
 function collide(x,y,type) {
